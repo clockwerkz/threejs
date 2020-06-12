103a246e51729d7976d7a51ca9fd021646296801
Carlos <clockwerkz@gmail.com>
2020-06-12 16:04:45 -0700

    trying to get objloader to work

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 var camera, scene, renderer;
 var geometry, material, mesh;
  
@@ -11,14 +9,20 @@ function init() {
     camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 5000 );
  
     scene = new THREE.Scene();
+    camera.position.z = 100;
     scene.background = new THREE.Color(0xddddd);
 
     const hlight = new THREE.AmbientLight(0x404040, 100);
     scene.add(hlight);
  
-    const loader = new GLTFLoader();
+    const loader = new THREE.GLTFLoader();
  
     renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setClearColor('#DDDDDD');
+    var light = new THREE.PointLight(0xFFFFFF, 1.4, 1000);
+    light.position.set(0,15,15);
+    scene.add(light);
+
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
     loader.load('../models/scene.gltf', function (obj) {
